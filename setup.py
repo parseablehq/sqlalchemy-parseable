d150889d339ddba7635b2a0ef559571f34724145
AdheipSingh <adheip1222@gmail.com>
2025-03-07 07:30:29 +0530

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sqlalchemy-parseable",
-    version="0.1.5",
+    version="0.1.6",
     description="SQLAlchemy dialect for Parseable",
     long_description="A SQLAlchemy dialect for interacting with Parseable, a high-performance log analytics database.",
     long_description_content_type="text/plain",
